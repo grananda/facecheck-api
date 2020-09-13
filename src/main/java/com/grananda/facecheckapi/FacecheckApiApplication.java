a273d1848323ea91ab01945fef1d898d2b63ecf4
mightyMouse <jfernandez74@gmai.com>
2020-09-13 20:01:14 +0200

    Repo init

---
+++ src/main/java/com/grananda/facecheckapi/FacecheckApiApplication.java
@@ -0,0 +1,13 @@
+package com.grananda.facecheckapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacecheckApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacecheckApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grananda/facecheckapi/FacecheckApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grananda.facecheckapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacecheckApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
