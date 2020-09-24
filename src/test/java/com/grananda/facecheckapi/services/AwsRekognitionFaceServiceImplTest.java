673cb319714d7a5cc98a5e6317bd601a8dbcfca7
mightyMouse <jfernandez74@gmai.com>
2020-09-24 11:47:51 +0200

    Disabled e2e AWS tests

--- src/test/java/com/grananda/facecheckapi/services/AwsRekognitionFaceServiceImplTest.java
+++ src/test/java/com/grananda/facecheckapi/services/AwsRekognitionFaceServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.grananda.facecheckapi.services;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import software.amazon.awssdk.services.rekognition.model.*;
@@ -11,6 +12,7 @@ import java.util.Collection;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@Disabled
 class AwsRekognitionFaceServiceImplTest extends BaseAwsRekognitionTest {
 
     @Autowired
